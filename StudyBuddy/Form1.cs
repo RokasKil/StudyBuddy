89b00d2a35c08adcb345fc505b48e4d2559ba7c8
roki5829 <rokas.kilinskas@mif.stud.vu.lt>
2019-09-11 18:41:53 +0300

    Test

--- StudyBuddy/Form1.cs
+++ StudyBuddy/Form1.cs
@@ -16,5 +16,10 @@ namespace StudyBuddy
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "!";
+        }
     }
 }
