c932273d8615327724c13833200734521809cb84
roki5829 <rokas.kilinskas@mif.stud.vu.lt>
2019-09-23 16:38:28 +0300

    Created empty form and added a value to the User entity

---
+++ StudyBuddy/ChangeProfilePictureForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyBuddy
+{
+    public partial class ChangeProfilePictureForm : Form
+    {
+        public ChangeProfilePictureForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeProfilePictureForm_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ChangeProfilePictureForm_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
