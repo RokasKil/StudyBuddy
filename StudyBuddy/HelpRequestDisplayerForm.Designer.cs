060a827dd49bd086b59331158e5f58e88bf7f85a
Agnė <agne3969@gmail.com>
2019-10-21 16:49:21 +0300

    HelpRequestDisplayerForm gerokai pagražinta, nebelieka cursor paspaudus ant teksto

--- StudyBuddy/HelpRequestDisplayerForm.Designer.cs
+++ StudyBuddy/HelpRequestDisplayerForm.Designer.cs
@@ -34,80 +34,105 @@
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.nameBox = new System.Windows.Forms.TextBox();
             this.profile = new System.Windows.Forms.Button();
+            this.writeMessageButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // descriptionBox
             // 
-            this.descriptionBox.Location = new System.Drawing.Point(9, 69);
-            this.descriptionBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.descriptionBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.descriptionBox.Location = new System.Drawing.Point(21, 85);
+            this.descriptionBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.descriptionBox.Multiline = true;
             this.descriptionBox.Name = "descriptionBox";
             this.descriptionBox.ReadOnly = true;
-            this.descriptionBox.Size = new System.Drawing.Size(272, 219);
+            this.descriptionBox.Size = new System.Drawing.Size(352, 243);
             this.descriptionBox.TabIndex = 0;
+            this.descriptionBox.Text = "Aprašymas";
             // 
             // categoryBox
             // 
-            this.categoryBox.Location = new System.Drawing.Point(9, 10);
-            this.categoryBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.categoryBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.categoryBox.Location = new System.Drawing.Point(21, 58);
+            this.categoryBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.categoryBox.Multiline = true;
             this.categoryBox.Name = "categoryBox";
             this.categoryBox.ReadOnly = true;
-            this.categoryBox.Size = new System.Drawing.Size(272, 27);
+            this.categoryBox.Size = new System.Drawing.Size(352, 23);
             this.categoryBox.TabIndex = 1;
+            this.categoryBox.Text = "Kategorija";
             // 
             // titleBox
             // 
-            this.titleBox.Location = new System.Drawing.Point(9, 41);
-            this.titleBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.titleBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.titleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleBox.Location = new System.Drawing.Point(21, 12);
+            this.titleBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.titleBox.Multiline = true;
             this.titleBox.Name = "titleBox";
             this.titleBox.ReadOnly = true;
-            this.titleBox.Size = new System.Drawing.Size(272, 24);
+            this.titleBox.Size = new System.Drawing.Size(352, 42);
             this.titleBox.TabIndex = 2;
+            this.titleBox.Text = "Pavadinimas";
             // 
             // pictureBox
             // 
-            this.pictureBox.Location = new System.Drawing.Point(284, 10);
-            this.pictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox.Location = new System.Drawing.Point(401, 12);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(197, 180);
+            this.pictureBox.Size = new System.Drawing.Size(230, 230);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 3;
             this.pictureBox.TabStop = false;
             // 
             // nameBox
             // 
-            this.nameBox.Location = new System.Drawing.Point(284, 195);
-            this.nameBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nameBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameBox.Location = new System.Drawing.Point(401, 246);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.nameBox.Multiline = true;
             this.nameBox.Name = "nameBox";
             this.nameBox.ReadOnly = true;
-            this.nameBox.Size = new System.Drawing.Size(198, 25);
+            this.nameBox.Size = new System.Drawing.Size(230, 30);
             this.nameBox.TabIndex = 4;
+            this.nameBox.Text = "Vardas Pavardė";
+            this.nameBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // profile
             // 
-            this.profile.Location = new System.Drawing.Point(287, 226);
+            this.profile.Location = new System.Drawing.Point(401, 282);
+            this.profile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.profile.Name = "profile";
-            this.profile.Size = new System.Drawing.Size(75, 23);
+            this.profile.Size = new System.Drawing.Size(105, 46);
             this.profile.TabIndex = 5;
             this.profile.Text = "Profilis";
             this.profile.UseVisualStyleBackColor = true;
             this.profile.Click += new System.EventHandler(this.profile_Click);
             // 
+            // writeMessageButton
+            // 
+            this.writeMessageButton.Location = new System.Drawing.Point(526, 282);
+            this.writeMessageButton.Margin = new System.Windows.Forms.Padding(4);
+            this.writeMessageButton.Name = "writeMessageButton";
+            this.writeMessageButton.Size = new System.Drawing.Size(105, 46);
+            this.writeMessageButton.TabIndex = 6;
+            this.writeMessageButton.Text = "Parašyti žinutę";
+            this.writeMessageButton.UseVisualStyleBackColor = true;
+            // 
             // HelpRequestDisplayerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(490, 292);
+            this.ClientSize = new System.Drawing.Size(653, 359);
+            this.Controls.Add(this.writeMessageButton);
             this.Controls.Add(this.profile);
             this.Controls.Add(this.nameBox);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.titleBox);
             this.Controls.Add(this.categoryBox);
             this.Controls.Add(this.descriptionBox);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "HelpRequestDisplayerForm";
             this.Text = "Help Request";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
@@ -124,5 +149,6 @@
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.TextBox nameBox;
         private System.Windows.Forms.Button profile;
+        private System.Windows.Forms.Button writeMessageButton;
     }
 }
--- StudyBuddy/HelpRequestDisplayerForm.cs
+++ StudyBuddy/HelpRequestDisplayerForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,11 @@ namespace StudyBuddy
     {
         LocalUser localUser;
         User user;
+
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+
+
         public HelpRequestDisplayerForm(LocalUser localUser, HelpRequest helpRequest, User user)
         {
             InitializeComponent();
@@ -25,6 +31,32 @@ namespace StudyBuddy
             pictureBox.ImageLocation = user.profilePictureLocation;
             this.localUser = localUser;
             this.user = user;
+
+            titleBox.MouseDown += TextBox_MouseDown;
+            titleBox.MouseUp += TextBox_MouseDown;
+            titleBox.GotFocus += TextBox_GotFocus;
+
+            categoryBox.MouseDown += TextBox_MouseDown;
+            categoryBox.MouseUp += TextBox_MouseDown;
+            categoryBox.GotFocus += TextBox_GotFocus;
+
+            descriptionBox.MouseDown += TextBox_MouseDown;
+            descriptionBox.MouseUp += TextBox_MouseDown;
+            descriptionBox.GotFocus += TextBox_GotFocus;
+
+            nameBox.MouseDown += TextBox_MouseDown;
+            nameBox.MouseUp += TextBox_MouseDown;
+            nameBox.GotFocus += TextBox_GotFocus;
+        }
+
+        private void TextBox_GotFocus(object sender, EventArgs args)
+        {
+            HideCaret(((TextBox)sender).Handle);
+        }
+
+        private void TextBox_MouseDown(object sender, EventArgs args)
+        {
+            HideCaret(((TextBox)sender).Handle);
         }
 
         private void profile_Click(object sender, EventArgs e)
