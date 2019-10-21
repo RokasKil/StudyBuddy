ceac6bf7ecb6748ee7a38da8ff0e9be643fbee94
Bernardas Labutis <bernardas.labutis@mif.stud.vu.lt>
2019-10-21 22:03:02 +0300

    UserReviewForma pilnai padaryta

---
+++ StudyBuddy/UserReviewForm.cs
@@ -0,0 +1,159 @@
+﻿using StudyBuddy.Entity;
+using StudyBuddy.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyBuddy
+{
+    public partial class UserReviewForm : Form
+    {
+        List<UserReview> userReviews;
+        LocalUser localUser;
+        User user;
+        Dictionary<string, User> users;
+        private ListViewColumnSorter listViewColumnSorter;
+        public UserReviewForm(LocalUser localUser)
+        {
+            InitializeComponent();
+            this.localUser = localUser;
+            progressBarKarma.Visible = false;
+            this.AcceptButton = buttonSortReviews;
+            listViewColumnSorter = new ListViewColumnSorter();
+            this.listViewUserReviews.ListViewItemSorter = listViewColumnSorter;
+        }
+
+        private void buttonSortReviews_Click(object sender, EventArgs e)
+        {
+            new UserGetter(localUser,
+                (status, user) =>
+                {
+                    this.Invoke((MethodInvoker)delegate //Grįžtama į main Thread !! SVARBU
+                    {
+                        if (status == UserGetter.GetStatus.Success) //Pavyko
+                        {
+                            this.user = user;
+                            progressBarKarma.Value = user.KarmaPoints;
+                            labelKarmaProgress.Text = user.username + " progresas " + progressBarKarma.Value
+                                + "/" + progressBarKarma.Maximum;
+                            progressBarKarma.Visible = true;
+                            LoadUserReviews();
+                        }
+                        else //Ne
+                        {
+                            progressBarKarma.Visible = false;
+                            MessageBox.Show(
+                            "Vartotojas " + richTextBoxUsername.Text + " nerastas",
+                            "Įvyko klaida",
+                            MessageBoxButtons.OK,  
+                           MessageBoxIcon.Error);
+                        }
+                    });
+                }
+                ).get(richTextBoxUsername.Text);
+        }
+
+        private void richTextBoxUsername_TextChanged(object sender, EventArgs e)
+        {
+                buttonSortReviews.Enabled = richTextBoxUsername.Text.Length > 0;
+        }
+
+        private void LoadUserReviews()
+        {
+            new UserReviewGetter(localUser,
+                (status, userReviews, users) =>
+                {
+                    this.Invoke((MethodInvoker)delegate //Grįžtama į main Thread !! SVARBU
+                    {
+                        if (status == UserReviewGetter.GetStatus.Success) // Pavyko
+                        {
+                            this.userReviews = userReviews;
+                            this.users = users;
+                            Console.WriteLine("success");
+                            bool karmaAdd = false;
+                            foreach (UserReview userReview in userReviews)
+                            {
+                                switch(userReview.karma)
+                                {
+                                    case 0:
+                                        karmaAdd = false;
+                                        break;
+                                    case 1:
+                                        karmaAdd = true;
+                                        break;
+                                }
+                                listViewUserReviews.Items.Add(
+                                    new ListViewItem(
+                                        new[] { 
+                                            userReview.username,
+                                            userReview.message,
+                                            ((karmaAdd)? "+" : "-"),
+                                            userReview.postDate.ToString()}));
+                            }
+                            ResizeColumnWidth();
+                        }
+                        else // Ne
+                        {
+                            Console.WriteLine("error");
+                            MessageBox.Show(
+                            "Nepavyko užkrauti",
+                            "Įvyko klaida",
+                            MessageBoxButtons.OK,
+                           MessageBoxIcon.Error);
+                        }
+                    });
+                }
+                ).get(true, user.username);
+
+            if (userReviews == null || users == null) // Dar nėra informacijos
+            {
+                return;
+            }
+        }
+        private void ResizeColumnWidth()
+        {
+            foreach (ColumnHeader column in listViewUserReviews.Columns)
+            {
+                column.Width = -2;
+            }
+        }
+
+        private void listViewUserReviews_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            //Determine if clicked column is already the column that is being sorted.
+            if (e.Column == listViewColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (listViewColumnSorter.Order == SortOrder.Ascending)
+                {
+                    listViewColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    listViewColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                listViewColumnSorter.SortColumn = e.Column;
+                listViewColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.listViewUserReviews.Sort();
+        }
+
+        private void listViewUserReviews_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            e.Cancel = true;
+            e.NewWidth = listViewUserReviews.Columns[e.ColumnIndex].Width;
+        }
+    }
+}
