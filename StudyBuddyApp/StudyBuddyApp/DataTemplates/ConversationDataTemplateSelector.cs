fea9e0d5d953d4571a0196f381397981cc1b8f0e
roki5829 <rokas.kilinskas@mif.stud.vu.lt>
2019-11-10 02:02:32 +0200

    Optimization

---
+++ StudyBuddyApp/StudyBuddyApp/DataTemplates/ConversationDataTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using StudyBuddyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StudyBuddyApp.DataTemplates
+{
+    public class ConversationDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate LeftTemplate { get; set; }
+        public DataTemplate RightTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((MessageModel)item).RightSide ? RightTemplate : LeftTemplate;
+        }
+    }
+}
