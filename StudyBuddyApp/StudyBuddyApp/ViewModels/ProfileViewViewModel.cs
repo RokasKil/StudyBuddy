b400999d8920d03d1f5480dada7d8b41b913c479
Bernardas Labutis <bernardas.labutis@mif.stud.vu.lt>
2019-11-13 17:32:29 +0200

    initial commit

---
+++ StudyBuddyApp/StudyBuddyApp/ViewModels/ProfileViewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using StudyBuddy.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyBuddyApp.ViewModels
+{
+    public class ProfileViewViewModel : BaseViewModel
+    {
+        public string ImageLocation { get; set; }
+        public int KarmaPoints { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public User User { get; set; }
+        public ProfileViewViewModel(User user)
+        {
+            this.User = user;
+            Title = user.Username + "Profilis";
+            this.ImageLocation = user.ProfilePictureLocation;
+            this.KarmaPoints = user.KarmaPoints;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+            this.UserName = user.Username;
+        }
+    }
+}
---
+++ StudyBuddyApp/StudyBuddyApp/Views/ProfileViewPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using StudyBuddy.Entity;
+using StudyBuddyApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudyBuddyApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfileViewPage : ContentPage
+    {
+        ProfileViewViewModel viewModel;
+        User user;
+        public ProfileViewPage(ProfileViewViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = this.viewModel = viewModel;
+            this.user = viewModel.User;
+        }
+
+        private async void ViewUserReviews_Clicked(object sender, EventArgs e)//TODO
+        {
+            //await Navigation.PushAsync(new ViewUserReviews(new ViewModels.ViewUserReviewsViewModel(user)));
+        }
+
+        private async void WriteUserReview_Clicked(object sender, EventArgs e)//TODO
+        {
+            //await Navigation.PushAsync(new WriteUserReview(new ViewModels.WriteUserReviewModel(user)));
+        }
+
+        private async void StartConversation_Clicked(object sender, EventArgs e)
+        {
+
+            if (clicked)
+                return;
+            clicked = true;
+
+            var conversationStarter = ConversationSystemManager.NewConversationStarter();
+            conversationStarter.StartConversationResult += (status, conversation, users) =>
+            {
+
+            };
+            /* await Navigation.PushModalAsync(
+                new NavigationPage(
+                    new ConversationPage(
+                        new ViewModels.ConversationViewModel(((new ConversationModel)e.Item).Conversation, users))));*/
+        }
+    }
+}
