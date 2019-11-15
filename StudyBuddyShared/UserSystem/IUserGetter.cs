d87624778892da0692aa0c0f381ccabdfcf35653
roki5829 <rokas.kilinskas@mif.stud.vu.lt>
2019-11-15 12:38:22 +0200

    User system interfaces

---
+++ StudyBuddyShared/UserSystem/IUserGetter.cs
@@ -0,0 +1,28 @@
+﻿using StudyBuddyShared.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyBuddyShared.UserSystem
+{
+    public enum UserGetStatus
+    {
+        InvalidPrivateKey,
+        Success,
+        JsonReadException,
+        FailedToConnect,
+        UnknownError,
+        FailedToFindUser,
+        UsernameEmpty,
+        FieldsMissing
+    }
+
+    public delegate void GetUserResultDelegate(UserGetStatus status, User user);
+
+    public interface IUserGetter : IPrivateKey
+    {
+        GetUserResultDelegate Result { get; set; } // Result delegate
+
+        void Get(string username); // Get user from username
+    }
+}
