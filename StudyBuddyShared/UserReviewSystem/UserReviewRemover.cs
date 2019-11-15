3fe3d45be4289994c97136633f8c851fff4d5f2e
roki5829 <rokas.kilinskas@mif.stud.vu.lt>
2019-11-15 12:54:17 +0200

    UserReview interfaces

---
+++ StudyBuddyShared/UserReviewSystem/UserReviewRemover.cs
@@ -0,0 +1,30 @@
+﻿using StudyBuddyShared.Entity;
+using StudyBuddyShared.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyBuddyShared.UserReviewSystem
+{
+    class UserReviewRemover : UserReviewManagerBase, IUserReviewRemover
+    {
+        public UserReviewRemover(LocalUser user) : this(user.PrivateKey)
+        {
+        }
+
+        public UserReviewRemover(string privateKey) : base(privateKey)
+        {
+            manager.RemoveUserReviewResult = (status, userReview) =>
+            {
+                Result?.Invoke(EnumConverter.Convert<UserReviewManageStatus>(status), userReview);
+            };
+        }
+
+        public UserReviewManageDelegate Result { get; set; }
+
+        public void Remove(UserReview userReview)
+        {
+            manager.removeUserReview(userReview);
+        }
+    }
+}
