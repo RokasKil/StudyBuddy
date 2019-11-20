ab7b930c0cb412cd11e5c5d91b2869a8cba04d1c
Bernardas Labutis <bernardas.labutis@mif.stud.vu.lt>
2019-11-20 17:10:46 +0200

    My favorite unit test

---
+++ UnitTests/EnumTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyBuddyShared.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EnumTest
+    {
+        public enum enum1 
+        { 
+            Success,
+            Failure,
+            Unknown
+        }
+
+        public enum enum2
+        {
+            Success,
+            Failure,
+            Unknown,
+            Feilas
+        }
+        [TestMethod]
+        public void ConvertEnumTest()
+        {
+            Assert.AreEqual(EnumConverter.Convert<enum2>(enum1.Unknown), enum2.Unknown);
+            Assert.AreNotEqual(EnumConverter.Convert<enum1>(enum1.Unknown), enum2.Unknown);
+        }
+    }
+}
