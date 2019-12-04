2149f8b054ca6d22a22d3a8e9c9fc57a4f3ff6a9
aubr5652 <augisbraziunas@mif.stud.vu.lt>
2019-12-04 09:00:10 +0200

    Created RankingGetter files (not tested yet)

---
+++ StudyBuddyShared/Network/RankingsGetter.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json.Linq;
+using StudyBuddyShared.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudyBuddyShared.Network
+{
+    class RankingsGetter
+    {
+        public enum GetStatus
+        {
+            InvalidPrivateKey,
+            Success,
+            JsonReadException,
+            FailedToConnect,
+            UnknownError,
+            FieldsMissing
+        }
+
+        public delegate void GetRankingsDelegate(GetStatus status, List<User> rankings);
+        public GetRankingsDelegate GetRankingsResult { get; set; }
+        public string PrivateKey { get; set; }
+        private Thread getRankingsThread;
+
+        public RankingsGetter() : this("") { }
+        public RankingsGetter(LocalUser user) : this(user.PrivateKey) { }
+        public RankingsGetter(string privateKey)
+        {
+            PrivateKey = privateKey;
+        }
+
+        public RankingsGetter(GetRankingsDelegate getRankingsResult) : this("", getRankingsResult) { }
+        public RankingsGetter(LocalUser user, GetRankingsDelegate getRankingsResult) : this(user.PrivateKey, getRankingsResult) { }
+        public RankingsGetter(string privateKey, GetRankingsDelegate getRankingsResult) : this(privateKey)
+        {
+            GetRankingsResult = getRankingsResult;
+        }
+
+        public void get()
+        {
+            if (getRankingsThread != null && getRankingsThread.IsAlive) // Jau vyksta užklausa
+            {
+                return;
+            }
+            getRankingsThread = new Thread(() => getLogic());
+            getRankingsThread.Start();
+        }
+
+        private void getLogic()
+        {
+            APICaller caller = new APICaller("getRankings.php").addParam("privateKey", PrivateKey);
+
+            JObject obj = caller.call();
+            //Console.WriteLine(obj);
+            if (obj["status"].ToString() == "success")
+            {
+                List<User> rankings = new List<User>();
+                obj["users"].ToList().ForEach((user) =>
+                {
+                    rankings.Add(new User
+                    {
+                        Username = obj["username"].ToString(),
+                        FirstName = obj["firstName"].ToString(),
+                        LastName = obj["lastName"].ToString(),
+                        KarmaPoints = obj["karmaPoints"].ToObject<int>(),
+                        IsLecturer = Convert.ToBoolean(obj["lecturer"].ToObject<int>()),
+                        ProfilePictureLocation = obj["profilePicture"].ToString()
+                    });
+                });
+                GetRankingsResult(GetStatus.Success, rankings);
+            }
+            else
+            {
+                GetStatus status = GetStatus.UnknownError;
+                if (!Enum.TryParse<GetStatus>(obj["message"].ToString(), out status))
+                {
+                    status = GetStatus.UnknownError;
+                }
+                GetRankingsResult(status, null);
+            }
+        }
+    }
+}
---
+++ StudyBuddyShared/UserSystem/RankingsGetter.cs
@@ -0,0 +1,37 @@
+﻿using StudyBuddyShared.Entity;
+using StudyBuddyShared.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace StudyBuddyShared.UserSystem
+{
+    public class RankingsGetter :IRankingsGetter
+    {
+        public RankingsGetter(LocalUser user) : this(user.PrivateKey)
+        {
+
+        }
+
+        public RankingsGetter(string privateKey)
+        {
+            this.PrivateKey = privateKey;
+            getter = new Network.RankingsGetter(privateKey, (status, helpRequests) =>
+            {
+                Result?.Invoke(EnumConverter.Convert<RankingsGetStatus>(status), helpRequests);
+            });
+        }
+
+        private Network.RankingsGetter getter;
+
+        public GetRankingsDelegate Result { get; set; }
+
+        public string PrivateKey { get; set; }
+
+        public void Get()
+        {
+            getter.get();
+        }
+    }
+}
