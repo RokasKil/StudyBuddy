224c7e106282aaed0095879d89b5468b431301fe
Bernardas Labutis <bernardas.labutis@mif.stud.vu.lt>
2019-12-07 14:19:17 +0200

    CommentSectionWebInterface

---
+++ StudyBuddyShared/CommentSystem/ICommentGetter.cs
@@ -0,0 +1,26 @@
+﻿using StudyBuddyShared.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyBuddyShared.CommentSystem
+{
+    public enum CommentGetStatus
+    {
+        InvalidPrivateKey,
+        Success,
+        JsonReadException,
+        FailedToConnect,
+        UnknownError,
+        FieldsMissing
+    }
+
+    public delegate void GetCommentDelegate(CommentGetStatus status, List<Comment> comments);
+
+    public interface ICommentGetter : IPrivateKey
+    {
+        GetCommentDelegate Result { get; set; } // Result delegate
+
+        void Get(int helpRequestID); //Get comments
+    }
+}
---
+++ StudyBuddyShared/CommentSystem/ICommentPoster.cs
@@ -0,0 +1,26 @@
+﻿using StudyBuddyShared.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyBuddyShared.CommentSystem
+{
+    public enum CommentManagerStatus
+    {
+        Success,
+        JsonReadException,
+        FailedToConnect,
+        UnknownError,
+        TitleMissing,
+        FieldsMissing
+    }
+
+    public delegate void CommentManagerDelegate(CommentManagerStatus status, Comment comment);
+
+    public interface ICommentPoster : IPrivateKey
+    {
+        CommentManagerDelegate Result { get; set; } // Return delegate
+
+        void PostComment(Comment comment); // Add category
+    }
+}
